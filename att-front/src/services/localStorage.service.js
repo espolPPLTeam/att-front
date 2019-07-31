5e722b35642d0b86aca69cd8987d8abc0c8fc8bc
edisonmora95 <edison_andre_9@hotmail.com>
2019-07-31 17:31:33 -0500

    LocalStorage service

---
+++ att-front/src/services/localStorage.service.js
@@ -0,0 +1,25 @@
+
+const TOKEN_KEY = 'x-access-token';
+
+/**
+ * Manage how Access Tokens are being stored and retreived from storage.
+ *
+ * Current implementation stores to localStorage. Local Storage should always be
+ * accessed through this instance.
+**/
+const TokenService = {
+  getToken() {
+    return localStorage.getItem(TOKEN_KEY);
+  },
+
+  saveToken(accessToken) {
+    localStorage.setItem(TOKEN_KEY, accessToken);
+  },
+
+  removeToken() {
+    localStorage.removeItem(TOKEN_KEY);
+  }
+
+};
+
+export { TokenService };
