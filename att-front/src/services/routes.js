c8c338ec736f2c6bb5d3903357252ae039130e96
edisonmora95 <edison_andre_9@hotmail.com>
2019-07-31 18:35:06 -0500

    Login service

--- att-front/src/services/routes.js
+++ att-front/src/services/routes.js
@@ -2,5 +2,5 @@ export const BASE_URL = `${process.env.VUE_APP_API_URL}`;
 
 export const routes = {
   //===== AUTHENTICATION =====//
-  LOGIN: "/user/login",
+  LOGIN: "/api/att/login",
 };
---
+++ att-front/src/services/user.service.js
@@ -0,0 +1,33 @@
+import { routes, BASE_URL } from "./routes";
+
+import ApiServcie from "./api.service";
+import { TokenService } from "./localStorage.service";
+
+const UserService = {
+
+  /**
+   * Request HTTP hacia API de login
+   *
+   * @param {Object} payload
+   * @param {String} payload.email Email del usuario
+   * @param {String} payload.clave Clave del usuario
+   */
+  async login(payload) {
+    const url = BASE_URL + routes.LOGIN;
+    const payloadData = {
+      email: payload.email,
+      clave: payload.clave,
+    };
+    try {
+      const response = await ApiServcie.postApi(url, payloadData);
+      //ApiServcie.setHeaderAuth(response.body.data.token);
+      TokenService.saveToken(response.body.data.token);
+      return Promise.resolve(response.body.data);
+    } catch(error) {
+      return Promise.reject(error);
+    }
+  }
+};
+
+
+export default UserService;
