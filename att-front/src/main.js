de2f1be9e0edafe7d1450bdeba086140b0fbcc4f
edisonmora95 <edison_andre_9@hotmail.com>
2019-07-31 17:03:46 -0500

    Added vuex module

--- att-front/src/main.js
+++ att-front/src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
+
 import router from "./router";
-import store from "./store";
-import "./registerServiceWorker";
+import store from "./store/";
+
+// import "./registerServiceWorker";
+
 import '@/assets/css/tailwind.css';
 import 'vue-sidebar-menu/dist/vue-sidebar-menu.css';
 import "./style.css";
